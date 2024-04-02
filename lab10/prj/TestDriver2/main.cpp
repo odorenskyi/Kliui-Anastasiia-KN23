db29a0e059caf7cc0c92ae985ed000ba11819ced
AnastasiaKlui <145664694+AnastasiaKlui@users.noreply.github.com>
2024-04-02 23:53:46 +0300

    remove exceed std namespace from task 10.2

--- lab10/prj/TestDriver2/main.cpp
+++ lab10/prj/TestDriver2/main.cpp
@@ -7,9 +7,9 @@ using namespace std;
 int main(){
     ofstream output_file("output.txt", ios_base::app);
     if (output_file.is_open()){
-        auto now = std::chrono::system_clock::now();
-        std::time_t time_stamp = std::chrono::system_clock::to_time_t(now);
-        output_file << std::ctime(&time_stamp);
+        auto now = chrono::system_clock::now();
+        std::time_t time_stamp = chrono::system_clock::to_time_t(now);
+        output_file << ctime(&time_stamp);
         output_file.close();
     }
     return 0;
