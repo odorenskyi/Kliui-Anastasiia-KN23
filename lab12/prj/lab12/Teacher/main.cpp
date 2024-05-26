48d8e3fade5cd178309a855f725df59e3e39f1b1
AnastasiaKlui <145664694+AnastasiaKlui@users.noreply.github.com>
2024-05-26 19:10:10 +0300

    add playing 100 beep sounds

--- lab12/prj/lab12/Teacher/main.cpp
+++ lab12/prj/lab12/Teacher/main.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
 
+#define BEEP_COUNT 100
+#define BEEP_REPEAT_DELAY 500
+
+void playBeepSounds(unsigned int count,
+                    unsigned int repeatDelay = BEEP_REPEAT_DELAY) {
+    for(auto n = 0; n < count; n++) {
+        std::cout << "beep " << n + 1 << '\a' << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(repeatDelay));
+    }
+}
+
+int main() {
+    playBeepSounds(BEEP_COUNT);
+    
+    return 0;
+}
