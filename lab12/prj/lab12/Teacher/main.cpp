0e47e4ff2fe5a557e5dfad9aaf31f723119d8f03
AnastasiaKlui <145664694+AnastasiaKlui@users.noreply.github.com>
2024-05-26 20:17:02 +0300

    implement unit testing for lab12

--- lab12/prj/lab12/Teacher/main.cpp
+++ lab12/prj/lab12/Teacher/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
+#include <filesystem>
+#include <vector>
+#include <fstream>
+#include <cmath>
+#include <limits>
+
+#include "ModulesKlui.h"
 
 #define BEEP_COUNT 100
 #define BEEP_REPEAT_DELAY 500
+const std::vector<std::filesystem::path> expectedSourceFilePath {"lab12", "prj", "main.cpp"};
+#define RESULT_FILE_NAME "TestResults.txt"
+#define ERROR_MESSAGE "Встановлені вимоги порядку виконання лабораторної роботи порушено!"
 
 void playBeepSounds(unsigned int count,
                     unsigned int repeatDelay = BEEP_REPEAT_DELAY) {
@@ -13,8 +23,77 @@ void playBeepSounds(unsigned int count,
     }
 }
 
+bool checkRequirements() {
+    auto sourceFilePath = std::filesystem::path(__FILE__);
+    auto pathElements = std::vector(sourceFilePath.begin(), sourceFilePath.end());
+    if(pathElements.size() >= 3) {
+        pathElements.erase(pathElements.begin(), pathElements.end() - 3);
+    }
+    
+    return pathElements == expectedSourceFilePath;
+}
+
+auto openResultFile() {
+    auto stream = std::ofstream(RESULT_FILE_NAME);
+    if(!stream.is_open()) {
+        std::cerr << "cannot open " << RESULT_FILE_NAME << std::endl;
+        exit(-1);
+    }
+    return stream;
+}
+
+bool doUnitTestCase(float radius,
+                    float height,
+                    float expectedVolume) {
+    auto object = new ClassLab12_Klui();
+    
+    object->setRadius(radius);
+    object->setHeight(height);
+    
+    auto actualVolume = object->calculateVolume();
+    delete object;
+    
+    return std::fabs(actualVolume - expectedVolume) < std::numeric_limits<float>::epsilon();
+}
+
+void doUnitTestCase(std::ostream &stream,
+                    unsigned int number,
+                    float radius,
+                    float height,
+                    float expectedVolume) {
+    stream << "Test Case " << number << ": ";
+    if(doUnitTestCase(radius, height, expectedVolume)) {
+        stream << "passed";
+    } else {
+        stream << "failed";
+    }
+    stream << std::endl;
+}
+
+void doUnitTesting() {
+    auto stream = openResultFile();
+    
+    doUnitTestCase(stream, 1, 10.0f, 11.5f, 100.0f);
+    doUnitTestCase(stream, 2, 10.0f, 11.5f, 100.0f);
+    doUnitTestCase(stream, 3, 10.0f, 11.5f, 100.0f);
+
+    stream.close();
+}
+
+void handleFailedRequirements() {
+    auto stream = openResultFile();
+
+    stream << ERROR_MESSAGE;
+    stream.close();
+}
+
 int main() {
     playBeepSounds(BEEP_COUNT);
+    if(checkRequirements()) {
+        doUnitTesting();
+    } else {
+        handleFailedRequirements();
+    }
     
     return 0;
 }
